6a7130762f521da3c7e91830fddeb89dc0ca9a14
Leroi <67743669+LeroiDev@users.noreply.github.com>
2025-02-06 21:08:23 +0200

    UserEntity-update-BaseApiController-created

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -6,4 +6,7 @@ public class AppUser
 {
     public int Id { get; set; }
     public required string UserName { get;set; } //nullable flag enabled in project file
+
+    public required byte[] PasswordHash { get; set; }
+    public required byte[] PasswordSalt { get; set; }
 }
