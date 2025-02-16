fbac7e6d3938765e89b8540fd12f3213596c77ff
Leroi <67743669+LeroiDev@users.noreply.github.com>
2025-02-16 22:54:02 +0200

    output-event-cancel-registration-old

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,4 +1,4 @@
-import { Component, input } from '@angular/core';
+import { Component, EventEmitter, input, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
@@ -10,6 +10,7 @@ import { FormsModule } from '@angular/forms';
 })
 export class RegisterComponent {
   usersFromHomeComponent = input.required<any>();
+  @Output() cancelRegister = new EventEmitter();
 
   model: any = {};
 
@@ -18,6 +19,6 @@ export class RegisterComponent {
   }
 
   cancel() {
-    console.log('cancelled registration');
+    this.cancelRegister.emit(false);
   }
 }
