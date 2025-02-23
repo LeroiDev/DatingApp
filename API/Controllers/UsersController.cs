c46c20c683c2bddd27bf9fa9dce5f9facb425104
Leroi <67743669+LeroiDev@users.noreply.github.com>
2025-02-24 01:09:23 +0200

    guard-causing-issue-submitting-form

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,3 +1,4 @@
+using System.Security.Claims;
 using API.Data;
 using API.DTOs;
 using API.Entities;
@@ -10,10 +11,11 @@ using Microsoft.EntityFrameworkCore;
 namespace API.Controllers;
 
 [Authorize]
-public class UsersController(IUserRepository userRepository) : BaseApiController 
+public class UsersController(IUserRepository userRepository, IMapper mapper) : BaseApiController
 {
-    [HttpGet] 
-    public async Task<ActionResult<IEnumerable<MemberDto>>>  GetUsers(){
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers()
+    {
         var users = await userRepository.GetMembersAsync();
         return Ok(users);  //type safety with action result feels weird here
     }
@@ -27,5 +29,21 @@ public class UsersController(IUserRepository userRepository) : BaseApiController
             return NotFound();
         }
         return user;
+    }
+
+    [HttpPut]
+    public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto)
+    {
+        var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+        if (username == null) return BadRequest("No username found in token");
+
+        var user = await userRepository.GetUserByUsernameAsync(username);
+        if (user == null) return BadRequest("Could not find user");
+
+        mapper.Map(memberUpdateDto, user);
+
+        if (await userRepository.SaveAllAsync()) return NoContent();
+
+        return BadRequest("Failed to update the user");
     }
 }
